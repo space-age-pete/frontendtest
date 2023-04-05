0b8803caf1f762e03fd19f46415bafd9a4440ff5
Josh Furlin <josh.furlin@gmail.com>
2023-04-04 21:19:10 -0500

    revamps page view to include all new components

---
+++ src/components/PageLayout.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="page-layout">
+        <slot />
+    </div>
+</template>
+
+<style>
+.page-layout {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,70 @@
 <script setup lang="ts">
 import { ref } from "vue"
 import ChessBoard from '@/components/ChessBoard.vue';
+import Sidebar from "@/components/Sidebar.vue";
+import PageLayout from "@/components/PageLayout.vue";
+import VariantChessBoard from "@/components/VariantChessBoard.vue";
 
 const boardFlipped = ref(false)
 
-function flipClick() {
-    boardFlipped.value = !boardFlipped.value
+const displayVariantBoard = ref(false)
+
+const squaresClicked = ref<string[]>([])
+
+function flipBoard() {
+  boardFlipped.value = !boardFlipped.value
 }
 
 function logSquares(coord: string): void {
-  console.log(coord)
+  squaresClicked.value.push(coord)
+}
+
+function switchBoardImplementation(): void {
+  reset();
+  boardFlipped.value = false;
+  displayVariantBoard.value = !displayVariantBoard.value
+}
+
+function reset() {
+  squaresClicked.value = []
 }
 
 </script>
 
 <template>
-  <main>
-    Chess
-  </main>
-    <button @click="flipClick">flip</button>
-  <ChessBoard :board-flipped="boardFlipped" @square-click="logSquares"/>
+  <PageLayout>
+    <div class="content">
+      <template v-if="!displayVariantBoard">
+        <ChessBoard :board-flipped="boardFlipped" @square-click="logSquares" />
+      </template>
+      <template v-else>
+        <VariantChessBoard :board-flipped="boardFlipped" @square-click="logSquares" />
+      </template>
+
+      <Sidebar :squares-clicked="squaresClicked" @flip-board-click="flipBoard" @reset="reset"
+        @switch-board-implementation="switchBoardImplementation" />
+    </div>
+  </PageLayout>
 </template>
+
+<style lang="scss">
+.content {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 50px;
+  width: 100%;
+  padding: 30px;
+  overflow: hidden;
+  height: fit-content;
+
+  @media (min-width: 960px) {
+    justify-content: center;
+    flex-direction: row;
+    align-items: flex-start;
+    height: 100%;
+  }
+}
+</style>
